a50dd832e2c03021fb7d076a40c53f3b8c710c32
lkace <lkacenas@gmail.com>
2022-10-05 15:44:37 +0300

    Initialise sayHelloLt from TestA in main()

--- src/main/java/lt/laurynas/demo/Main.java
+++ src/main/java/lt/laurynas/demo/Main.java
@@ -3,6 +3,8 @@ package lt.laurynas.demo;
 public class Main {
 
     public static void main(String[] args) {
+
+      TestA.sayHelloLT();
         System.out.println("Hello world!!!");
     }
 }
--- src/main/java/lt/laurynas/demo/TestA.java
+++ src/main/java/lt/laurynas/demo/TestA.java
@@ -2,7 +2,8 @@ package lt.laurynas.demo;
 
 public class TestA {
 
-    public void sayHelloLT(){
+    public static void sayHelloLT(){
         System.out.println("Sveikas pasauli!!!");
+
     }
 }
